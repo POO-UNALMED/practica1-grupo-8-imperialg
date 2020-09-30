ac5aa3c45ba53268bbfbf367be7b42e51eb3e19f
Anderson Gutierrez <69688234+Anderson-Gt@users.noreply.github.com>
2020-09-30 16:35:02 -0500

    Ui

--- src/uiMain/Ui.java
+++ src/uiMain/Ui.java
@@ -1,16 +1,30 @@
 package uiMain;
+import java.util.*;
 import gestorAplicacion.*;
 import BaseDatos.*;
 
 public class Ui {
-	public void ingresarConsola(String nombre, Boolean uso, ){
-
+	Scanner entrada = new Scanner(System.in);
+	public void ingresarConsola(String nombre, boolean uso, String color, boolean estado, String version, int almacenamiento){
+		nombre = entrada.next();
+		uso = entrada.nextBoolean();
+		color = entrada.next();
+		estado = entrada.nextBoolean();
+		version = entrada.next();
+		almacenamiento = entrada.nextInt();
 	}
-	public void ingresarJuego(){
-
+	public void ingresarJuego(String nombre, boolean uso, int pegi, String plataforma, String genero){
+		nombre = entrada.next();
+		uso = entrada.nextBoolean();
+		pegi = entrada.nextInt();
+		plataforma = entrada.next();
+		genero = entrada.next();
 	}
-	public void ingresarPeriferico(){
-
+	public void ingresarPeriferico(String nombre, boolean uso, boolean estado, String plataforma){
+		nombre = entrada.next();
+		uso = entrada.nextBoolean();
+		estado = entrada.nextBoolean();
+		plataforma = entrada.next();
 	}
 
 	public static void main(String[] Args) {
